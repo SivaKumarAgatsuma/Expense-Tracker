c54be4e2382aac810d97447cd76ee28b8f6ccdcb
Siva Kumar <117735455+SivaKumarAgatsuma@users.noreply.github.com>
2023-05-03 00:07:35 +0530

    Add files via upload

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,31 @@
+import React,{useState} from "react";
+import './NewExpense.css';
+import ExpenseForm from "./ExpenseForm";
+const NewExpense=(props)=>
+{
+    const [isEditing,setisEditing]=useState(false);
+    const saveExpenseDataHandler=(enteredExpenseData)=>
+    {
+        const expenseData={
+            ...enteredExpenseData,id: Math.random().toString()
+        }
+        props.onAddExpense(expenseData);
+        isEditing(false);
+    }
+    const startEditingHandler=()=>
+    {
+        setisEditing(true);
+    }
+    const stopEditingHandler=()=>
+    {
+        setisEditing(false);
+    }
+
+    return(
+        <div className="new-expense">
+            {!isEditing && <button onClick={startEditingHandler}>Add New Expense</button>}
+            {isEditing && <ExpenseForm onCancel={stopEditingHandler} onSaveExpenseData={saveExpenseDataHandler}/>}
+        </div>
+    )
+}
+export default NewExpense;
